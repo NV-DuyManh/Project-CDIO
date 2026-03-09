ebcac7fffbe6b379838fd94f8fe2965226ddb760
trantin12345 <trantin505ad@gmail.com>
2026-03-09 20:50:46 +0700

    Cap nhat giao dien lich su va nut them vao gio hang

--- CDIO/config/config.py
+++ CDIO/config/config.py
@@ -6,7 +6,7 @@ import os
 DB_CONFIG = {
     "host":     "localhost",
     "user":     "root",
-    "password": ".TranThinh270400",
+    "password": "123456789",
     "database": "cdio",
     "charset":  "utf8mb4"
 }
